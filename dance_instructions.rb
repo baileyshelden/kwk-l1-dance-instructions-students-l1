242c2cb4d39359d334b93fb23cf8284ac43cae62
baileyshelden <bailey7748@gmail.com>
2018-06-05 17:11:18 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,65 @@
 
 # Code your methods below
+
+puts "Plant legs far apart, bend knees slightly and keep posture loose"
+
+def skip_step_left 
+  puts "Lower left foot to the ground."
+  puts "Bounce keft foot back up slightly, kicking it a few inches back."
+  puts
+end 
+
+def skip_step_right
+  puts "lower right foot to the ground."
+  puts "Bounce right foot back up slightly, kicking it a few inches back."
+end
+  
+def bob_the_reigns
+  puts "Holding your arms out in front of you, straigh and at chest level."
+  puts "Cross your right wrist over your left and hold them together."
+  puts "Lift your arms up and down in a loose bouncing movement, in time with the beat of the song."  
+end 
+
+def lasso 
+  puts "Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left."
+  puts "Lift right arm up and point right elbow diagonally to the right."
+  puts "Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso."
+end
+  
+def step_RLRR
+  puts "Step Right."
+  puts "Step Left."
+  puts "Step Right."
+  puts "Step Right."
+end
+
+def step_LRLL
+  puts "Step Left."
+  puts "Step Right."
+  puts "Step Left."
+  puts "Step Left"
+end 
+
+puts "Plant legs far apart, bend knees slightly and keep posture loose."
+step_LRLL
+step_RLRR
+bob_the_reigns while skip_step_right
+bob_the_reigns while skip_step_right
+bob_the_reigns while skip_step_right
+bob_the_reigns while skip_step_right
+bob_the_reigns while skip_step_right
+bob_the_reigns while skip_step_right
+bob_the_reigns while skip_step_right
+bob_the_reigns while skip_step_right
+lasso while skip_step_left
+lasso while skip_step_left
+lasso while skip_step_left
+lasso while skip_step_left
+lasso while skip_step_left
+lasso while skip_step_left
+lasso while skip_step_left
+
+
+
+
+
---
+++ greeeting.rb
@@ -0,0 +1,11 @@
+local_greeting = "Hey duude"
+your_name = "Kaia"
+my_name = "Bailey"
+time_of_day = "Afternoon"
+
+
+def greeting 
+  puts "#{local_greeting} #{your name}. I'm #{my name}, How's your #{time_of_day}"
+end
+
+greeting
